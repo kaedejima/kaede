a55247d7c94415d04fd0c3b416b6c61a35c2cdee
“kaedejima” <frogtoadie@gmail.com>
2020-10-21 19:38:22 +0900

    hamburger js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-const loveSubmit = document.querySelector('.sendLove');
-const sendForm = document.querySelector('.sendForm');
-const printArea = document.querySelector('.printArea');
-const printThx = document.querySelector('#thxMes');
-let resetButton;
-let i = 1;
-
-loveSubmit.addEventListener('click', checkLove);
-sendForm.addEventListener('click', thxMessage);
-function checkLove() {
-    printArea.textContent += 'I love you, too: ' + i;
-    i++;
-}
-
-function thxMessage() {
-    printThx.textContent += ' Thanks for contacting me! ';
-}
+$(function () {
+    $("#hamburger").on('click', function () {
+        // $(this).toggleClass("rotated-image");
+        $(this).toggleClass("rotate");
+    })
+});
