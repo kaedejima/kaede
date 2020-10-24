07178ad942d6730a14d7ca789468989e13614f40
“kaedejima” <frogtoadie@gmail.com>
2020-10-24 11:57:10 +0900

    fin pure html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// ******************** ONLOAD ********************
+window.onload = function () {
+};
+
+$(function () {
+    $("#footer").load("../view/footer.html");
+    $('#header').load('../view/header.html');
+    // path
+    var path = location.pathname;
+    if (path == '/index.html') {
+        $('#body').load('../view/index.html');
+    }
+});
+
+// ******************** NAVIGATION BAR ********************
 $(function () {
     $("#hamburger").on('click', function () {
         // $(this).toggleClass("rotated-image");
