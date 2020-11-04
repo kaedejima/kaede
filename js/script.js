7cea8ab5ec70e3c486974266ff49b27a02799971
“kaedejima” <frogtoadie@gmail.com>
2020-11-04 23:27:01 +0900

    experiences added, card

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
 // ******************** ONLOAD ********************
 window.onload = function () {
 };
-
-$(function () {
-    $("#footer").load("../view/footer.html");
-    $('#header').load('../view/header.html');
-    // path
-    var path = location.pathname;
-    if (path == '/index.html') {
-        $('#body').load('../view/index.html');
-    }
-});
-
 // ******************** NAVIGATION BAR ********************
 $(function () {
     $("#hamburger").on('click', function () {
