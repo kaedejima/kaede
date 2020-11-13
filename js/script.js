b3d236954f731be191dcf97bd79e5a413576de6f
“kaedejima” <frogtoadie@gmail.com>
2020-11-14 00:10:13 +0900

    language switch for index

--- js/script.js
+++ js/script.js
@@ -8,6 +8,38 @@ $(function () {
         $(this).toggleClass("rotate");
     })
 });
+// ******************** CHANGE THEME ********************
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+})();
+// ******************** LANGUAGE SWITCH ********************
+$(function () {
+    $(".section").on('click', function () {
+        // $(this).toggleClass("rotated-image");
+        $(".lang-jpn").toggleClass("inactive");
+        $(".lang-eng").toggleClass("inactive");
+    })
+});
 // ******************** WORK PAGES ********************
 $(function () {
     $(".work-more").on('click', function () {
