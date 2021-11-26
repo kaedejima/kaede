e498c2f13ba339a195a1635494567afb5b1a1421
kaedejima <frogtoadie@gmail.com>
2021-11-26 23:01:43 +0900

    refactor: directory works

--- js/includes.js
+++ js/includes.js
@@ -6,6 +6,13 @@ function navbar(page) {
     document.getElementById("nav-" + page).classList.toggle("active")
   })();
 };
+function navbarWorks(page) {
+  var navbar = "<nav class='navbar navbar-expand-lg navbar-dark static-top fixed-top'> <div class='container'> <a class='navbar-brand' href='./index.html'> <img id='kdlogo' src='../images/kdlogo.png' alt=''> </a> <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation' id='hamburger'> <span class='navbar-toggler-icon'></span> </button> <div class='collapse navbar-collapse' id='navbarResponsive'> <ul class='navbar-nav ml-auto'> <li class='nav-item'> <a class='nav-link' id='nav-home' href='../index.html'>Home </a> </li> <li class='nav-item'> <a class='nav-link' id='nav-about' href='../index.html#about'>About</a> </li> <li class='nav-item'> <a class='nav-link' id='nav-skills' href='../index.html#skills'>Skills</a> </li> <li class='nav-item'> <a class='nav-link' id='nav-works' href='../index.html#works'>Works </a> </li> <li class='nav-item'> <a class='nav-link' id='nav-sayhi' href='#say-hi'>Say-Hi!</a> </li> </ul> </div> </div> </nav>";
+  document.write(navbar);
+  (function () {
+    document.getElementById("nav-" + page).classList.toggle("active")
+  })();
+};
 
 function top_area() {
   var top_area = "<div class='jumbotron jumbotron-extend'> <div class='container jumbotron-container'> <div id='top-sq' class='mx-auto text-center'> <div class='py-5'> <h4>KD</h4> <p>1999 May 23</p> <h6>Studying Computer Science and Communication Engineering at Waseda University</h6> </div> </div> </div> </div>";
@@ -18,7 +25,11 @@ function say_hi() {
 }
 
 function footer() {
-  var footer = "<footer class='font-small pt-4'> <div class='container text-center text-md-left'> <a href='./index.html'> <img id='kdlogo-name' src='./images/kdlogo-name-sm.png' class='d-block mx-auto mb-4'> </a> <!-- <p>Busy doing nothing...</p> --> <div class='row justify-content-center mb-4'> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='./index.html#about' class='text-light'>About</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='./index.html#skills' class='text-light'>Skills</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='work-home.html' class='text-light'>Works</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='#say-hi' class='text-light'>Say-Hi!</a> </p> </h6> </div> </div> </div> <!-- Copyright --> <div class='footer-copyright text-center py-3 back-brown'>© 2021 KD</div> <!-- Copyright --> </footer>";
+  var footer = "<footer class='font-small pt-4'> <div class='container text-center text-md-left'> <a href='./index.html'> <img id='kdlogo-name' src='./images/kdlogo-name-sm.png' class='d-block mx-auto mb-4'> </a> <!-- <p>Busy doing nothing...</p> --> <div class='row justify-content-center mb-4'> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='./index.html#about' class='text-light'>About</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='./index.html#skills' class='text-light'>Skills</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='works/home.html' class='text-light'>Works</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='#say-hi' class='text-light'>Say-Hi!</a> </p> </h6> </div> </div> </div> <!-- Copyright --> <div class='footer-copyright text-center py-3 back-brown'>© 2021 KD</div> <!-- Copyright --> </footer>";
+  document.write(footer);
+}
+function footerWorks() {
+  var footer = "<footer class='font-small pt-4'> <div class='container text-center text-md-left'> <a href='./index.html'> <img id='kdlogo-name' src='../images/kdlogo-name-sm.png' class='d-block mx-auto mb-4'> </a> <!-- <p>Busy doing nothing...</p> --> <div class='row justify-content-center mb-4'> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='../index.html#about' class='text-light'>About</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='../index.html#skills' class='text-light'>Skills</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='home.html' class='text-light'>Works</a> </p> </h6> </div> <div class='col-xs-6 col-sm-2 mx-auto'> <!-- Links --> <h6 class='text-uppercase font-weight-bold'> <p> <a href='#say-hi' class='text-light'>Say-Hi!</a> </p> </h6> </div> </div> </div> <!-- Copyright --> <div class='footer-copyright text-center py-3 back-brown'>© 2021 KD</div> <!-- Copyright --> </footer>";
   document.write(footer);
 }
 
